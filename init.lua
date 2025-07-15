6d89bbcf313aadf76c2ad292e56c3923b812fd15
DevRobertoMartinez <217862081+DevRobertoMartinez@users.noreply.github.com>
2025-07-15 11:26:03 -0400

    Adds Angularls

--- init.lua
+++ init.lua
@@ -688,6 +688,7 @@ require("lazy").setup({
 				-- pyright = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+				angularls = {},
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
 				--    https://github.com/pmizio/typescript-tools.nvim
