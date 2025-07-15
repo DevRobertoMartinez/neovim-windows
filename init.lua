4d17e4bee82ba49cfbe30a30b7c3fe63094f5fe2
DevRobertoMartinez <217862081+DevRobertoMartinez@users.noreply.github.com>
2025-07-15 15:35:48 -0400

    Adds emmet_ls & emmet_language_server

--- init.lua
+++ init.lua
@@ -689,6 +689,8 @@ require("lazy").setup({
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				angularls = {},
+				emmet_ls = {},
+				emmet_language_server = {},
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
 				--    https://github.com/pmizio/typescript-tools.nvim
